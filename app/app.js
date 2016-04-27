9bd9cc3e928861817fb1cbf9d2fea97453aec572
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-27 14:36:04 -0500

    Assignment Commit

--- app/app.js
+++ app/app.js
@@ -1,18 +1,17 @@
 import 'es6-promise';
 import 'whatwg-fetch';
 
-// navigator.geolocation.getCurrentPosition(function(position) {
-//   do_something(position.coords.(34.74648), position.coords.-92.289609);
-// });
+navigator.geolocation.getCurrentPosition(function(position) {
+  do_something(position.coords.latitude, position.coords.longitude);
+});
 
 
 
-.fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=<34.74648>&lon=<-92.289609>&APPID=<75d92f502972a0f9ea05c1163aa5cbf4>&units=imperial`)
-.then((response)function {
-  console.log(response);
+.fetch('http://api.openweathermap.org/data/2.5/forecast/daily?lat=<34.74648>&lon=<-92.289609>&APPID=<75d92f502972a0f9ea05c1163aa5cbf4>&units=imperial')
+.then((response) => {
   return response.jason();
-})
+});
 
-.then((response)function {
+.then((response) => {
   console.log(response);
 });
