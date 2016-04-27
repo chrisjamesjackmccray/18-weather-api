d02f6b85090b2f6da7b02ff5734f16cf5ede0170
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-27 14:08:40 -0500

    Commit

--- app/app.js
+++ app/app.js
@@ -1,2 +1,18 @@
 import 'es6-promise';
 import 'whatwg-fetch';
+
+navigator.geolocation.getCurrentPosition(function(position) {
+  do_something(position.coords.(34.74648), position.coords.-92.289609);
+});
+
+
+
+.fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=<34.74648>&lon=<-92.289609>&APPID=<75d92f502972a0f9ea05c1163aa5cbf4>&units=imperial`)
+.then((response)function {
+  console.log(response);
+  return response.jason();
+})
+
+.then((response)function {
+  console.log(response);
+});
