c88e58b0a466365853ab2c688353612b4ae1aa3c
James McCray <chrisjamesjackmccray@gmail.com>
2016-04-29 16:21:15 -0500

    Commit

--- app/app.js
+++ app/app.js
@@ -2,16 +2,23 @@ import 'es6-promise';
 import 'whatwg-fetch';
 
 navigator.geolocation.getCurrentPosition(function(position) {
-  do_something(position.coords.latitude, position.coords.longitude);
-});
+  console.log(position);
+  fetch(`http://api.openweathermap.org/data/2.5/forecast/daily?lat=${position.coords.latitude}&lon=${position.coords.longitude}&APPID=75d92f502972a0f9ea05c1163aa5cbf4&units=imperial`)
+  .then((response) => {
+    return response.json();
+  })
+  .then((response) => {
+    console.log(response);
 
+    document.querySelector(".city").textContent = response.city.name;
+    document.querySelector(".fewclouds").textContent = response.list[0].clouds;
+    document.querySelector(".deg").textContent = response.list[0].temp.day;
+    document.querySelector(".dayone").textContent = response.list[0].dt;
+    document.querySelector(".daytwo").textContent = response.list[1].dt;
+    document.querySelector(".daythree").textContent = response.list[2].dt;
+    document.querySelector(".dayfour").textContent = response.list[3].dt;
+    document.querySelector(".dayfive").textContent = response.list[4].dt;
+    document.querySelector(".daysix").textContent = response.list[5].dt;
+  });
 
-
-.fetch('http://api.openweathermap.org/data/2.5/forecast/daily?lat=<34.74648>&lon=<-92.289609>&APPID=<75d92f502972a0f9ea05c1163aa5cbf4>&units=imperial')
-.then((response) => {
-  return response.jason();
-});
-
-.then((response) => {
-  console.log(response);
 });
